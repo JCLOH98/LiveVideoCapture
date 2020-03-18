7ce47504e455fac866c7d44845f130572e69bdee
JCLOH98 <jcloh98@gmail.com>
2020-03-19 04:08:52 +0800

    multi clients can access to the live streaming webcam

--- LVC2.py
+++ LVC2.py
@@ -5,30 +5,29 @@ from flask import Flask
 from flask import Response
 from flask import render_template
 
-import threading
+from threaded_camera import WebcamVideoStream 
+#from threaded_camera2 import CameraStream
 
 app = Flask(__name__);
+cap = WebcamVideoStream().start();
+#cap = CameraStream().start();
 
 def gen():
-	cap = cv.VideoCapture(0);
-	while cap.isOpened():
-		_,frame = cap.read();
-		mirrorframe = cv.flip(frame,1);
-		flag, encodedImage = cv.imencode(".jpg",mirrorframe);
+	while cap:
+		outputframe = cap.read();
+		(flag, encodedImage) = cv.imencode(".jpg",outputframe);
 		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encodedImage) + b'\r\n')
-		#cv.imshow("LiveVideo",frame);
-		#if (cv.waitKey(1) == ord("q")):
-			#break;
-	#cv.destroyAllWindows();
-	cap.release();
+		
 
 @app.route("/")
 def index():
 	return render_template("index.php");
+
 @app.route("/video_feed")
 def video_feed():
 	return Response(gen(),mimetype="multipart/x-mixed-replace; boundary=frame");
 
 if __name__ == "__main__":
-	app.run(debug=True,port=80,host="0.0.0.0",threaded=True);
+	app.run(debug=True,port=80,host="0.0.0.0",threaded=True,use_reloader=False);
 
+cap.stop();
